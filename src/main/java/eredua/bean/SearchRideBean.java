88eeb2d052e2ffeb0c207c15996ea89acfef87cc
Egoitz <eladron002@ikasle.ehu.eus>
2025-11-15 16:54:42 +0100

    FetchRide egitura eta funtzionamendua

---
+++ src/main/java/eredua/bean/SearchRideBean.java
@@ -0,0 +1,81 @@
+package eredua.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import domain.Ride;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("searchRideBean")
+@SessionScoped
+public class SearchRideBean implements Serializable{
+	private String departingCity;
+	private String arrivalCity;
+	private Date rideDate;
+	private List<Ride> results;
+
+	private List<String> departingCities;
+	private List<String> arrivalCities;
+
+	public void init() {
+		this.departingCities = FacadeBean.getBusinessLogic().getDepartCities();
+	}
+	
+	public void onDepartingCityChange() {
+		this.arrivalCities = FacadeBean.getBusinessLogic().getDestinationCities(departingCity);
+	}
+	
+	public void searchRides() {
+		this.results = FacadeBean.getBusinessLogic().getRides(departingCity, arrivalCity, rideDate);
+	}
+
+	public String getDepartingCity() {
+		return departingCity;
+	}
+
+	public void setDepartingCity(String departingCity) {
+		this.departingCity = departingCity;
+	}
+
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+
+	public Date getRideDate() {
+		return rideDate;
+	}
+
+	public void setRideDate(Date rideDate) {
+		this.rideDate = rideDate;
+	}
+
+	public List<Ride> getResults() {
+		return results;
+	}
+
+	public void setResults(List<Ride> results) {
+		this.results = results;
+	}
+
+	public List<String> getDepartingCities() {
+		return departingCities;
+	}
+
+	public void setDepartingCities(List<String> departingCities) {
+		this.departingCities = departingCities;
+	}
+
+	public List<String> getArrivalCities() {
+		return arrivalCities;
+	}
+
+	public void setArrivalCities(List<String> arrivalCities) {
+		this.arrivalCities = arrivalCities;
+	}
+}
