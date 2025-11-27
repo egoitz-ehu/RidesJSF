2143f7099b55e351480f4c352bf63fbadaa5ccc3
Egoitz <eladron002@ikasle.ehu.eus>
2025-11-27 20:32:30 +0100

    Bidaiak lortu

--- src/main/java/dao/RideDAO.java
+++ src/main/java/dao/RideDAO.java
@@ -1,5 +1,27 @@
 package dao;
 
-public class RideDAO {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import domain.Ride;
+
+public class RideDAO {
+	private EntityManager entityManager;
+
+	public List<Ride> getRidesByValues(String departingCity, String arrivalCity, Date rideDate) {
+		if (departingCity == null || arrivalCity == null || rideDate == null)
+			return new ArrayList<Ride>();
+		try {
+			return entityManager.createQuery(
+					"SELECT r FROM Ride r WHERE r.departingCity=:departingCity AND r.arrivalCity=:arrivalCity AND r.rideDate=:rideDate",
+					Ride.class).setParameter("departingCity", departingCity).setParameter("arrivalCity", arrivalCity)
+					.setParameter("rideDate", rideDate).getResultList();
+		} catch (Exception e) {
+			System.out.println(e);
+			return new ArrayList<Ride>();
+		}
+	}
 }
--- src/main/java/domain/Ride.java
+++ src/main/java/domain/Ride.java
@@ -1,5 +1,79 @@
 package domain;
 
-public class Ride {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ride {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String departingCity;
+	private String arrivalCity;
+	private Date rideDate;
+	private int availableSeats;
+	private double pricePerSeat;
+
+	public Ride() {
+	}
+	
+	public Ride(String departingCity, String arrivalCity, Date rideDate, int availableSeats, double pricePerSeat) {
+		this.departingCity = departingCity;
+		this.arrivalCity = arrivalCity;
+		this.rideDate = rideDate;
+		this.availableSeats = availableSeats;
+		this.pricePerSeat = pricePerSeat;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDepartingCity() {
+		return departingCity;
+	}
+
+	public void setDepartingCity(String departingCity) {
+		this.departingCity = departingCity;
+	}
+
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+
+	public Date getRideDate() {
+		return rideDate;
+	}
+
+	public void setRideDate(Date rideDate) {
+		this.rideDate = rideDate;
+	}
+
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+
+	public double getPricePerSeat() {
+		return pricePerSeat;
+	}
+
+	public void setPricePerSeat(double pricePerSeat) {
+		this.pricePerSeat = pricePerSeat;
+	}
 }
