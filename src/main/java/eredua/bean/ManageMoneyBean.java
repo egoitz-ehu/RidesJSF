5de5d6c9dfdb6051ef4963d9e1cd6d98669c4e41
Egoitz <eladron002@ikasle.ehu.eus>
2025-12-03 19:02:17 +0100

    diru mugimenduak martxan

---
+++ src/main/java/eredua/bean/ManageMoneyBean.java
@@ -0,0 +1,78 @@
+package eredua.bean;
+
+import java.io.Serializable;
+import java.util.ResourceBundle;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named("manageMoneyBean")
+@SessionScoped
+public class ManageMoneyBean implements Serializable {
+	@Inject
+	private AuthBean authBean;
+	
+	private double currentBalance;
+	private double amountToAdd;
+	private double amountToWithdraw;
+	
+	public void addMoney() {
+		ResourceBundle bundle = ResourceBundle.getBundle("messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+		try {
+			FacadeBean.getBusinessLogic().depositMoney(authBean.getUser().getEmail(), amountToAdd);
+			String msg = bundle.getString("money.successDeposit");
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", msg));
+	        this.currentBalance += amountToAdd;
+		} catch (Exception e) {
+			String msg = bundle.getString("money.errorWithdraw");
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", msg));
+		}
+	}
+	
+	public void withdrawMoney() {
+		ResourceBundle bundle = ResourceBundle.getBundle("messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+		try {
+			boolean egoera = FacadeBean.getBusinessLogic().withdrawMoney(authBean.getUser().getEmail(), amountToWithdraw);
+			if(!egoera) {
+		        String msg = bundle.getString("money.errorWithdraw");
+		        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", msg));
+			} else {
+				String msg = bundle.getString("money.successWithdraw");
+		        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", msg));
+		        this.currentBalance -= amountToWithdraw;
+			}
+		} catch (Exception e) {
+			String msg = bundle.getString("money.errorWithdraw");
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", msg));
+		}
+	}
+
+	public double getCurrentBalance() {
+		return currentBalance;
+	}
+
+	public void setCurrentBalance(double currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+
+	public double getAmountToAdd() {
+		return amountToAdd;
+	}
+
+	public void setAmountToAdd(double amountToAdd) {
+		this.amountToAdd = amountToAdd;
+	}
+
+	public double getAmountToWithdraw() {
+		return amountToWithdraw;
+	}
+
+	public void setAmountToWithdraw(double amountToWithdraw) {
+		this.amountToWithdraw = amountToWithdraw;
+	}
+	
+	
+}
