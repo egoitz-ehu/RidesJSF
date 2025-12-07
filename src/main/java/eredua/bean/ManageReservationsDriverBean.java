8b25daa09f0a3fc328b0229a7335a5f521efe509
Egoitz <eladron002@ikasle.ehu.eus>
2025-12-07 20:38:03 +0100

    gidariaren erreserbak lortu martxan

--- src/main/java/eredua/bean/ManageReservationsDriverBean.java
+++ src/main/java/eredua/bean/ManageReservationsDriverBean.java
@@ -7,16 +7,20 @@ import java.util.List;
 import domain.Reservation;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Named("manageReservationsDriverBean")
 @RequestScoped
 public class ManageReservationsDriverBean implements Serializable{
+	@Inject
+	private AuthBean authBean;
+	
 	private List<Reservation> reservations;
 	
 	@PostConstruct
 	public void init() {
-		this.reservations = new LinkedList<Reservation>();
+		this.reservations = FacadeBean.getBusinessLogic().getDriverReservations(authBean.getUser().getEmail());
 	}
 
 	public List<Reservation> getReservations() {
